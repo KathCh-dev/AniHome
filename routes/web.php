f37910e564ca1954288ea94b7a1443cf31d9b51b
Kathleen <kathleen.chaiffre.pro@gmail.com>
2025-07-16 10:08:29 +0200

    mise à jour des fichiers par rapport à la branche us-14-Admin, je décide de merge les deux branches pour gagner du temps

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,10 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [AuthController::class, 'register'])-> name('register');
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::get('clientDashboard', [AuthController::class, 'clientDashboard'])->name('clientDashboard');
+Route::post('/register', [AuthController::class, 'doRegister'])->name('doRegister');
+Route::get('/clientLogin', [AuthController::class, 'clientLogin'])->name('clientLogin');
+Route::post('/clientLogin', [AuthController::class, 'clientDoLogin'])->name('clientDoLogin');
+Route::get('/clientDashboard', [AuthController::class, 'clientDashboard'])->name('clientDashboard');
+Route::get('/adminLogin', [AuthController::class, 'adminLogin'])->name('adminLogin');
+Route::post('/adminLogin', [AuthController::class, 'adminDoLogin'])->name('adminDoLogin');
+Route::get('/adminDashboard', [AuthController::class, 'adminDashboard'])->name('adminDashboard');
