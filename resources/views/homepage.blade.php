607407cb43df5786b5fa9726a53873f817ddd18a
Kathleen <kathleen.chaiffre.pro@gmail.com>
2025-08-02 15:26:28 +0200

    mise en page de la homepage avec CSS manuel

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,51 +3,56 @@
 @section('content')
 
 
-<section class="bg-purple-100 p-5 flex-column justify-center" id="welcome-section">
-        <h1 class="text-center text-indigo-900 font-extrabold text-5xl m-5">Bienvenue chez Ani'Home</h1>
-
-        <img src="\..\..\images\homepage-chien-accueil.png" alt="Photo d'un chien qui court sur la plage.">
-
-        <a href="{{ route('register') }}"><button class="bg-purple-400 text-center text-yellow-50 font-bold text-base border border-3 border-purple-900 m-2 p-2 hover:bg-purple-900 button" id="create-account">Créer un compte</button></a>
+    <section class="bg-purple-100 p-5" id="welcome-section">
+        <div class="flex-column center">
+            <h1 class="text-center text-indigo-900 font-extrabold text-5xl m-5">Bienvenue chez Ani'Home</h1>
+    
+            <img src="\..\..\images\homepage-chien-accueil.png" alt="Photo d'un chien qui court sur la plage." class="place-self-center" id="welcome-dog-image">
+    
+            <a href="{{ route('register') }}"><button class="bg-purple-400 text-center text-yellow-50 font-bold text-base border border-3 border-purple-900 m-2 p-2 hover:bg-purple-900 button" id="create-account">Créer un compte</button></a>
+        </div>
     </section>
 
-    <section class="p-5" id="services-section">
+    <section class="p-5 padding-bottom" id="services-section">
         <h2 class="text-center text-indigo-900 font-extrabold text-2xl m-5">Découvrez nos services</h2>
         <div class="flex">
-            <div class="services-card">
-                <h3>Promenade</h3>
-                <img src="\..\..\images\promenade.png" alt="Photo d'un Cavalier King Charles.">
+            <div class="services-card text-center max-w-sm mx-auto">
+                <h3 class="text-indigo-900 font-extrabold">Promenade</h3>
+                <br>
+                <img src="\..\..\images\promenade.png" alt="Photo d'un Cavalier King Charles." class="services-image" id="walk-image">
             </div>
-            <div class="services-card">
-                <h3>Toilettage</h3>
-                <img src="\..\..\images\toilettage.png" alt="Photo d'un cochon d'Inde dans son bain.">
+            <div class="services-card text-center max-w-sm mx-auto">
+                <h3 class="text-indigo-900 font-extrabold">Toilettage</h3>
+                <br>
+                <img src="\..\..\images\toilettage.png" alt="Photo d'un cochon d'Inde dans son bain." class="services-image" id="grooming-image">
             </div>
-            <div class="services-card">
-                <h3>Dressage</h3>
-                <img src="\..\..\images\dressage.png" alt="Photo d'un Labrador Chocolat qui fait le beau pour recevoir une friandise.">
+            <div class="services-card text-center max-w-sm mx-auto">
+                <h3 class="text-indigo-900 font-extrabold">Dressage</h3>
+                <br>
+                <img src="\..\..\images\dressage.png" alt="Photo d'un Labrador Chocolat qui fait le beau pour recevoir une friandise." class="services-image" id="training-image">
             </div>
         </div>
     </section>
 
-    <section class="bg-purple-100 p-5" id="rooms-section">
+    <section class="bg-purple-100 p-5 padding-bottom" id="rooms-section">
         <h2 class="text-center text-indigo-900 font-extrabold text-2xl m-5">Découvrez nos séjours en chambre</h2>
         <div class="flex">
-            <div class="max-w-sm mx-auto bg-white rounded-2xl shadow-md p-6 space-y-4 flex-column room-card" id="room1">
-                <h3>Chambre 1 :</h3>
+            <div class="max-w-sm mx-auto rounded-2xl shadow-md p-6 space-y-4 flex-column room-card center" id="room1">
+                <h3 class="text-indigo-900 font-extrabold">Chambre 1 :</h3>
                 <img src="\..\..\images\logo-maison.png" alt="Logo d'une maison.">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, sunt iure! Iure ducimus aspernatur perspiciatis possimus aut! Eaque recusandae, repellat assumenda minima tenetur, est ullam qui perferendis sed enim voluptates!</p>
                 <button class="bg-purple-400 text-center text-yellow-50 font-bold text-base border border-3 border-purple-900 m-2 p-2 hover:bg-purple-900 button">Réserver</button>
             </div>
 
-            <div class="max-w-sm mx-auto bg-white rounded-2xl shadow-md p-6 space-y-4 flex-column room-card" id="room2">
-                <h3>Chambre 2 :</h3>
+            <div class="max-w-sm mx-auto rounded-2xl shadow-md p-6 space-y-4 flex-column room-card center" id="room2">
+                <h3 class="text-indigo-900 font-extrabold">Chambre 2 :</h3>
                 <img src="\..\..\images\logo-maison.png" alt="Logo d'une maison.">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, sunt iure! Iure ducimus aspernatur perspiciatis possimus aut! Eaque recusandae, repellat assumenda minima tenetur, est ullam qui perferendis sed enim voluptates!</p>
                 <button class="bg-purple-400 text-center text-yellow-50 font-bold text-base border border-3 border-purple-900 m-2 p-2 hover:bg-purple-900 button">Réserver</button>
             </div>
 
-            <div class="max-w-sm mx-auto bg-white rounded-2xl shadow-md p-6 space-y-4 flex-column room-card" id="room3">
-                <h3>Chambre 3 :</h3>
+            <div class="max-w-sm mx-auto rounded-2xl shadow-md p-6 space-y-4 flex-column room-card center" id="room3">
+                <h3 class="text-indigo-900 font-extrabold">Chambre 3 :</h3>
                 <img src="\..\..\images\logo-maison.png" alt="Logo d'une maison.">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, sunt iure! Iure ducimus aspernatur perspiciatis possimus aut! Eaque recusandae, repellat assumenda minima tenetur, est ullam qui perferendis sed enim voluptates!</p>
                 <button class="bg-purple-400 text-center text-yellow-50 font-bold text-base border border-3 border-purple-900 m-2 p-2 hover:bg-purple-900 button">Réserver</button>
